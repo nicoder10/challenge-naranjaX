53fbf3c39dbe49cb8cb4f520b36f56b25870f43f
Nicolas Martino <martinonicolas10@gmail.com>
2021-08-05 17:37:28 -0300

    Added cityModel

--- server/routes/cities.js
+++ server/routes/cities.js
@@ -1,8 +1,31 @@
 const express = require('express');
 const router = express.Router();
+const cityModel = require('../models/cityModel');
 
 router.get('/test', (req, res) => {
     res.send({ msg: 'Cities test' });
-})
+});
+
+router.get('/all', (req, res) => {
+    cityModel.find({})
+        .then((data) => {
+            res.send(data)
+        })
+        .catch((err) => console.log(err));
+});
+
+router.post('/', (req, res) => {
+    const newCity = new cityModel({
+        name: req.body.name,
+        country: req.body.country,
+        img: req.body.img
+    })
+    
+    newCity.save()
+        .then((city) => res.send(city))
+        .catch((err) => res.status(500).send('Internal server error'));
+        
+});
+
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -3,6 +3,12 @@ const app = express();
 const port = process.env.PORT || 5000;
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const db = require('./keys').mongoURI; // import database credentials
+const mongoose = require('mongoose'); 
+
+mongoose.connect(db, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true})
+    .then(() => console.log('MongoDB conection established'))
+    .catch((err) => console.log(err));
 
 // MIDDLEWARES
 app.use(express.json({ extended: true }))
